3d07ec3730b1033fef2af7d3510d1d8f298c3077
Giri Nugraha <girinuaha@gmail.com>
2017-10-26 11:40:28 +0700

    rubah name jadi nama

--- project-maven-125/src/main/java/com/xsis/training125/controller/Address.java
+++ project-maven-125/src/main/java/com/xsis/training125/controller/Address.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Address {
 
-	private String name;
+	private String nama;
 	private String postalCode;
 	
 	public String getName() {
-		return name;
+		return nama;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.nama = name;
 	}
 	public String getPostalCode() {
 		return postalCode;
