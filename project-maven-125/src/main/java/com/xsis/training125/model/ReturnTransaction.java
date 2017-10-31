da189c5bb52753524068302053711931e6c5d47d
rusli <xsis1>
2017-10-31 13:55:38 +0700

    add

--- project-maven-125/src/main/java/com/xsis/training125/model/ReturnTransaction.java
+++ project-maven-125/src/main/java/com/xsis/training125/model/ReturnTransaction.java
@@ -1,35 +1,46 @@
 package com.xsis.training125.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@Table(name = "RETURN TRANSACTION")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
 public class ReturnTransaction {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+	@OneToOne(fetch = FetchType.LAZY, mappedBy="return_transaction")
+	private Employee employee;
+	@OneToOne(fetch = FetchType.LAZY, mappedBy="return_transaction")
+	private Customer customer;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="return_transaction")
+	private List<Book> books;
 	private Date returnDate;
 	
 	public ReturnTransaction() {}
 
-	public ReturnTransaction(int id, Date returnDate) {
+	public ReturnTransaction(int id, Employee employee, Customer customer, List<Book> books, Date returnDate) {
 		super();
 		this.id = id;
+		this.employee = employee;
+		this.customer = customer;
+		this.books = books;
 		this.returnDate = returnDate;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Date getReturnDate() {
-		return returnDate;
-	}
-
-	public void setReturnDate(Date returnDate) {
-		this.returnDate = returnDate;
-	}
-	
 	
 }
